33060c580b2f2b6658f684e0030794958f655354
Munyama <githarajoel@gmail.com>
2025-12-28 09:49:34 +0300

    WEEKLY ASSIGNMENTS

---
+++ J77-10542-2024-java/project java/calculatetotalcost.java
@@ -0,0 +1,34 @@
+// Program to compute total cost
+/*
+*Name: Joel Githara
+*Reg No: J77-10542-2024
+*Description: the program helps to calculate total cost used on a vacation 
+*/
+import java.util.Scanner;
+public class calculatetotalcost {
+    public static double calculatetotalcost(double accomodationcost, double mealcost, double activitycost){
+        return accomodationcost +mealcost +activitycost;
+    }
+  public static void main(String[] args){
+      Scanner scanner=new Scanner(System.in);
+
+      System.out.println("Enter accomodation cost: ");
+      double accomodationcost= scanner.nextDouble();
+
+      System.out.println("Enter meals cost: ");
+      double mealcost=scanner.nextDouble();
+
+      System.out.println("Enter activities cost:");
+      double activitycost=scanner.nextDouble();
+
+      // calculate total cost
+
+      double totalcost = calculatetotalcost(accomodationcost,mealcost,activitycost);
+
+      System.out.println("Total vacation cost:$"+totalcost);
+
+
+  }
+
+}
+
---
+++ J77-10542-2024-java/project java/fitnessapp.java
@@ -0,0 +1,32 @@
+// Program for a fitness app tracker 
+/*
+*Name: Joel Githara
+*Reg No: J77-10542-2024
+*Description: it helps users to track their fitness level 
+*/
+import java.util.Scanner;
+public class fitnessapp {
+    public static String getfitnesslevel(int steps) {
+        if (steps >= 10000) {
+            return "excellent";
+        } else if (steps >= 7000) {
+            return "Good";
+        } else if (steps >= 4000) {
+            return "Average";
+        } else {
+            return "poor";
+
+        }
+
+    }
+    public static void main (String[] args){
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Enter your steps for today:");
+        int steps = scanner.nextInt();
+        String totalsteps = getfitnesslevel(steps);
+
+        System.out.println("your fitness level: "+ totalsteps);
+    }
+}
+
